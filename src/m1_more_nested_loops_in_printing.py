b30d9764662c6848b607474005e94cbbf5536836
Lucas D'Alesio <dalesil@rose-hulman.edu>
2019-01-28 00:53:45 -0500

    in progress

--- src/m1_more_nested_loops_in_printing.py
+++ src/m1_more_nested_loops_in_printing.py
@@ -4,8 +4,8 @@ in the context of PRINTING on the CONSOLE.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Lucas D'Alesio.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -56,7 +56,7 @@ def triangle_right_justified(r):
     For purposes of "lining up", assume r is a single digit.
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # HINT: Do the following problem FIRST, then convert x's to spaces:
@@ -74,10 +74,12 @@ def triangle_right_justified(r):
     #   in this or the other problems in this module, as doing so would
     #   defeat the goal of providing practice at loops within loops.
     # -------------------------------------------------------------------------
-    for j in range(r):
-        for k in range(r):
+    for j in range(r, 0, -1):
+        for n in range(j - 1):
+            print(' ', end='')
+        for k in range(r - j + 1):
             print(k+1, end='')
-        print("")
+        print('')
 
 def run_test_triangle_upside_down():
     """ Tests the    triangle_upside_down    function. """
@@ -125,6 +127,8 @@ def triangle_upside_down(r):
     for j in range(r, 0, -1):
         for k in range(j):
             print(k + 1, end='')
+        for n in range(j - 1):
+            print(' ', end='')
         print()
 
 def run_test_vee():
